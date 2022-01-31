92bae10625873ffd5cd756357c841b5a54d65c05
Shruthi Reddy <shruthi.reddy@qentelli.com>
2022-01-31 10:35:48 +0530

    react hooks home component change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,43 +4,35 @@ import Search from '../../components/Search/Search.component';
 import './Home.styles.css'
 import {connect} from 'react-redux'
 import { searchMonster } from '../../redux/monster/monster.action';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 
-class Home extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            monsters: [],
-        }
-    }
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ monsters: users }));
-    }
 
-    handleSearch = (event)=>{
-        const {searchField} = this.props
+const Home = (props)=> {
+    const [data, setData] = useState([])
+    const {searchName} = props
+    console.log(data)
+    useEffect(async () => {
+      const result = await axios('https://jsonplaceholder.typicode.com/users')
+      setData(result.data)
+    },[])
+    const filteredMonsters = data.filter((monster) => monster.name.toLowerCase().includes(searchName.toLowerCase()))
+    const handleSearch = (event)=>{
+        const {searchField} = props
         searchField(event.target.value)
     }
-   
-    render(){
-        const {monsters} = this.state
-        const {searchName} = this.props
-        const filteredMonsters = monsters.filter((monster) => monster.name.toLowerCase().includes(searchName.toLowerCase()))
-        return(
-            <>
-            <div className='App'>
-                <h1>Monsters Rolodex</h1>
-               
-                <Search handleSearch={this.handleSearch} />
-                <CardList monsters={filteredMonsters} />
-                
-            </div>
-            </>
-        )
-    }
-}
+    return(
+    <>
+    <div className='App'>
+        <h1>Monsters Rolodex</h1>
+       
+        <Search handleSearch={handleSearch} />
+        <CardList monsters={filteredMonsters} />
+        
+    </div>
+    </>
+)}
 
 const mapDispatchToProps = dispatch =>({
     searchField: username => dispatch(searchMonster(username)) 
